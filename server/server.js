127ab348bf0a90547081142f42b664d652a1dd4f
roudaynah <roudaynah@gmail.com>
2026-02-18 14:25:53 -0600

    Added server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT  = 3100;
+const PORT = 3100;
+
+const path = require('path');
+const result = require('dotenv').config({ path: path.resolve(__dirname, '../server/.env.development') });
+
+const {Pool, Client} = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {rejectUnauthorized: false}
+});
 
 app.use(cors());
-app.get("/api/home", (req,res)=> {
-    res.json({message : "Welcome to the Wallet Wizard Project"});
+
+
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Welcome to Wallet Wizard Project" });
+});
+
+
+app.get("/api/transaction", async (req, res) => {
+  let client
+  try {
+    client = await pool.connect();
+    console.log('Got a connection from the pool');
+    const resp = await client.query('SELECT * FROM transaction');
+    res.json(resp.rows);
+  } catch (err) {
+    console.error(err);
+    res.json({error: err});
+  } finally {
+    client?.release();
+  }
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+    });
